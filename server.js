248ddb1fe5b7c6b8a0beecb32e379beb24f3aa86
JeranDev <jeranjb@gmail.com>
2020-11-19 11:34:03 -0700

    add nodemailer skeleton

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
 
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 mongoose.connect(process.env.MONGO_CONNECTION, {
   useNewUrlParser: true,
@@ -61,6 +64,38 @@ app.get('/store', (req, res) => {
 
 app.get('/success', (req, res) => {
   res.render('success', {})
+})
+
+app.post('/form', (req, res) => {
+  async function main() {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'mail.jerandev.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.JERANDEV_EMAIL,
+        pass: process.env.JERANDEV_PASSWORD,
+      },
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false,
+      },
+    })
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `"${req.body.name}" <${req.body.email}>`, // sender address
+      to: 'jerandev@outlook.com', // list of receivers
+      subject: 'Hellovit Form Submission', // Subject line
+      text: req.body.message, // plain text body
+      html: `<b>${req.body.message}</b>`, // html body
+    })
+
+    console.log('Message sent: %s', info.messageId)
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  }
+  main().catch(console.error)
 })
 
 const PORT = process.env.PORT || 5000
